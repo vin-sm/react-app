062a5858bd509ba6a3c3d7b0f700636c1b37bb20
vin-sm <98011328+vin-sm@users.noreply.github.com>
2024-01-05 14:33:31 +0530

    removed commented code

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,27 +1,3 @@
-// import React, { useState } from 'react';
-
-// const Description = ({ text }) => {
-//   const [showMore, setShowMore] = useState(false);
-
-//   const toggleShowMore = () => {
-//     setShowMore(!showMore);
-//   };
-
-//   const wordArray = text.split(' ');
-//   const displayText = showMore ? text : wordArray.slice(0, maxWords).join(' ');
-
-//   return (
-//     <div>
-//       <p>{showMore ? text : `${text.slice(0, 00)}...`}</p>
-//       <button onClick={toggleShowMore}>
-//         {showMore ? 'Show Less' : 'Show More'}
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Description;
-
 import React, { useState } from 'react';
 
 const Description = ({ text, maxCharacter }) => {
@@ -37,12 +13,10 @@ const Description = ({ text, maxCharacter }) => {
     <div>
       <p>
         {displayText}
-        {/* {wordArray.length > maxWords && ( */}
           <>
             {' '}
             <button onClick={toggleShowMore}>{showMore ? 'Show Less' : 'Show More'}</button>
           </>
-        {/* )} */}
       </p>
     </div>
   );
